66ff377b22389e5c8308b58cf95231b0ab20f74e
sajithamaduranga <sajithamaduranga2001@gmail.com>
2026-02-14 17:22:29 +0530

    sajiha

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
       <div className="heroContainer">
         <div className="heroSection">
-          <h1 className="heroTitle">Welcome To Spice Stock</h1>
+          <h1 className="heroTitle">sajitha</h1>
         </div>
 
       
